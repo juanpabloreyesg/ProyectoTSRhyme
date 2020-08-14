1acb1275149668c99884f28ab4bdd876038ffbd9
juanpabloreyesg <jp.reyes39@uniandes.edu.co>
2020-08-14 17:00:45 -0500

    Tarea 3 - con modificaciones

--- aprendiz.ts
+++ aprendiz.ts
@@ -1,3 +1,5 @@
+import { Curso } from './curso.js'
+
 export enum NivelEducativo {
     BACHILLERATO = "Bachillerato",
     UNIVERSITARIO = "Universitario",
@@ -8,8 +10,19 @@ export enum NivelEducativo {
 export class Aprendiz {
 
   
-    constructor(public nombres: string, public apellidos: string, public avatar: string, public edad: number,public nivelEducativo: NivelEducativo){
+    constructor(public nombres: string, public apellidos: string, public avatar: string, public edad: number,public nivelEducativo: NivelEducativo, public cursos: Curso[]){
        
+    }
+
+    public darCursosCertificados():number{
+        let total_cursos_ap = 0;
+        for (let curso of this.cursos)
+        {
+            if(curso.certificado){
+                total_cursos_ap += 1;
+            }                  
+        }
+        return total_cursos_ap;
     }
 
 }
--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 import { Aprendiz, NivelEducativo } from './aprendiz.js';
+import { Curso } from './curso.js';
 
-export const ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, NivelEducativo.POSGRADO);
+var cursos = [new Curso("Prácticas esenciales para el agilismo", 20, 90, true, 2019),
+    new Curso("Ingeniería de software para la web", 15, 99, true, 2018),
+    new Curso("Pruebas automatizadas", 25, 50, false, 2020),
+    new Curso("Principios de diseño y arquitectura", 30, 75, true, 2020)];
+const ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, NivelEducativo.POSGRADO, cursos);
 
 let aprendizTbody: HTMLElement = document.getElementById('aprendiz')!;
+let cursosTbody: HTMLElement = document.getElementById('cursos')!;
+let estadisticasTbody: HTMLElement = document.getElementById('estadisticas')!;
 
 mostrarDatosAprendiz(ap);
+mostrarCursosAprendiz(ap);
+mostrarEstadisticas(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
     let tbodyElemento = document.createElement("tbody");
@@ -30,4 +39,29 @@ function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
     </tr>`
     aprendizTbody.appendChild(tbodyElemento); 
 
+}
+
+function mostrarCursosAprendiz(aprendiz: Aprendiz): void{
+    let cursos: Curso[] = aprendiz.cursos;
+    let tbodyElement = document.createElement("tbody");
+    for(let index = 0; index < cursos.length; index++){
+        let curso: Curso = cursos[index];
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${curso.nombre}</td>
+                               <td>${curso.horas}</td>
+                               <td>${curso.calificacion}</td>
+                               <td>${curso.certificado}</td>
+                               <td>${curso.anio}</td>`;
+        tbodyElement.appendChild(trElement);
+    }
+    cursosTbody.appendChild(tbodyElement);
+}
+
+function mostrarEstadisticas(ap: Aprendiz): void{
+    let promedio = ap.darCursosCertificados();
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `<td><b>Cursos certificados:</b></td>
+                            <td>${promedio}</td>`;
+    estadisticasTbody.appendChild(trElement);
+                
 }
